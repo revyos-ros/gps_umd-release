73bd4a16c65af2b2bec0c0ce5290f4767532c1c0
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-06-14 10:32:33 +0800

    Imported upstream version '2.0.3' of 'upstream'

--- gpsd_client/src/client.cpp
+++ gpsd_client/src/client.cpp
@@ -86,13 +86,7 @@ namespace gpsd_client
       if (!gps_->waiting(1e6))
         return;
 
-      // Read out all queued data and only act on the latest
-      gps_data_t* p = NULL;
-      while (gps_->waiting(0))
-      {
-        p = gps_->read();
-      }
-
+      gps_data_t* p = gps_->read();
 #else
       gps_data_t *p = gps->poll();
 #endif
